7696abaf295e446965d48c9b4e0355c78f57dfdf
Micky373 <michaeltamirie@gmail.com>
2022-10-11 13:24:41 +0300

    Finished week_4 practice notebook

--- scripts/dataframe.py
+++ scripts/dataframe.py
@@ -3,6 +3,9 @@ from IPython.display import display
 from sklearn.metrics import mutual_info_score
 import matplotlib.pyplot as plt
 import seaborn as sns 
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 def data_frame_refining(df):
     df.columns = df.columns.str.lower().str.replace(' ', '_')
@@ -72,5 +75,69 @@ def corr_matrix(df,title:str,save_as):
     plt.savefig(f'../charts/{save_as}')
     plt.show
 
+def tpr_fpr_dfr(y_val,y_pred):
+    scores = []
+    threshold = np.linspace(0,1,101)
+
+    for t in threshold:
+        actual_positive = y_val == 1
+        actual_negative = y_val == 0
+
+        predict_positive = y_pred >= t
+        predict_negative = y_pred < t
+
+        tp = (actual_positive & predict_positive).sum()
+        tn = (actual_negative & predict_negative).sum()
+
+        fn = (predict_negative & actual_positive).sum()
+        fp = (predict_positive & actual_negative).sum()
+
+        scores.append((t,tp,tn,fn,fp))
+
+    columns = ['threshold','tp','tn','fn','fp']
+    df_scores = pd.DataFrame(scores,columns=columns)
+
+    df_scores['fpr'] = df_scores.fp / (df_scores.tn + df_scores.fp)
+    df_scores['tpr'] = df_scores.tp / (df_scores.fn + df_scores.tp)
+
+    return df_scores
 
 
+def plot_tpr_fpr_graph(df):
+    plt.plot(df.threshold,df.tpr,label='TPR')
+    plt.plot(df.threshold,df.fpr,label='FPR')
+    plt.legend()
+
+def plot_three_tpr_fpr(df1,df2,df3,label_1,label_2,label_3):
+    plt.plot(df1.fpr,df1.tpr,label= label_1)
+
+    plt.plot(df2.fpr,df2.tpr,label= label_2)
+
+    plt.plot(df3.fpr,df3.tpr,label= label_3)
+
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.legend()
+
+def train(df,y_train,categorical_cols,numerical_cols,C=1.0):
+
+    dicts = df[categorical_cols + numerical_cols].to_dict(orient = 'records')
+
+    dv = DictVectorizer(sparse=False)
+    X_train = dv.fit_transform(dicts)
+
+    model = LogisticRegression(C=C,max_iter=1000)
+    model.fit(X_train,y_train)
+
+
+    return dv, model
+
+def predict(df,categorical_cols,numerical_cols,dv,model):
+
+    dicts = df[categorical_cols + numerical_cols].to_dict(orient = 'records')
+
+    X = dv.transform(dicts)
+
+    y_pred = model.predict_proba(X)[:,1]
+
+    return y_pred
